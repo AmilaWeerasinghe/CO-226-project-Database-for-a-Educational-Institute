30047a25eb20120d260fc112e712d8fbf8c3e7bd
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-04-08 22:49:26 +0530

    Register successfully implemented using PHP local host database

--- form.php
+++ form.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+$_SESSION['message'] = '';
+$mysqli=new mysqli('localhost','root','','educationalnstitute');
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    if($_POST['password']==$_POST['confirmpassword']){
+        $username= $mysqli->real_escape_string($_POST['username']);
+        $email= $mysqli->real_escape_string($_POST['email']);
+        $password=md5($_POST['password']);
+        
+        $_SESSION['username']=$username;
+
+        $sql="INSERT INTO users (username,email,password)"."VALUES('$username','$email','$password')";
+
+        if($mysqli->query($sql)==true){
+            $_SESSION['message']="Registrstion success $username!";
+            header("location:home.html");
+        }
+        else{
+            $_SESSION['message']="Could not load to database!";
+
+        }
+
+
+
+    }
+    else{
+        $_SESSION['message']=" Passwords don't match!";
+
+    }
+}
+
+
+?>
+
 <link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
 <link rel="stylesheet" href="form.css" type="text/css">
 <div class="body-content">
@@ -9,7 +46,6 @@
       <input type="email" placeholder="Email" name="email" required />
       <input type="password" placeholder="Password" name="password" autocomplete="new-password" required />
       <input type="password" placeholder="Confirm Password" name="confirmpassword" autocomplete="new-password" required />
-      <div class="avatar"><label>Select your avatar: </label><input type="file" name="avatar" accept="image/*" required /></div>
       <input type="submit" value="Register" name="register" class="btn btn-block btn-primary" />
     </form>
   </div>
